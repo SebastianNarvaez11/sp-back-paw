2906c694612960c954b61cf293d4d2024288b1ae
Sebastian Narvaez <sebastiannarvaez400@gmail.com>
2020-08-28 16:03:32 -0500

    grade: configuracion de exportar importar

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from .models import Grade
 
 # Register your models here.
+
+
+class GradeResource(resources.ModelResource):
+    class Meta:
+        model = Grade
+
+
+class GradeAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    resource_class = GradeResource
+
+admin.site.register(Grade,GradeAdmin)
