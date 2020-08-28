58a4fff43423eb848602a97e98b58be5643e6b69
Sebastian Narvaez <sebastiannarvaez400@gmail.com>
2020-08-28 15:51:47 -0500

    admin: configuracion de exportacion

--- users/admin.py
+++ users/admin.py
@@ -4,13 +4,26 @@ from import_export.admin import ImportExportModelAdmin
 from .models import Admin, User, Student
 
 # Register your models here.
+
+
 class UserResource(resources.ModelResource):
     class Meta:
         model = User
 
+
 class UsersAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     resource_class = UserResource
 
+
+class StudentResource(resources.ModelResource):
+    class Meta:
+        model = Student
+
+
+class StudentAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    resource_class = StudentResource
+
+
 admin.site.register(Admin)
 admin.site.register(User, UsersAdmin)
-admin.site.register(Student)
+admin.site.register(Student, StudentAdmin)
