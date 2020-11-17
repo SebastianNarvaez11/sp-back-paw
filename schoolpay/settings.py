03c66466d8696e9490cacb060d92505f9a3985e6
SebastianNarvaez11 <narvaez.jhoan@correounivalle.edu.co>
2020-11-17 11:49:41 -0500

    'update2'

--- schoolpay/settings.py
+++ schoolpay/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    # 'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
