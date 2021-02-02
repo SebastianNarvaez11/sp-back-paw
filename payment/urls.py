9330e67e11c7c0bfae14e05465f8724e3159642b
SebastianNarvaez11 <narvaez.jhoan@correounivalle.edu.co>
2021-02-02 08:11:34 -0500

    test

--- payment/urls.py
+++ payment/urls.py
@@ -3,11 +3,15 @@ from .views import *
 
 urlpatterns = [
     # PAGOS PARA ESTUDIANTES
-    path('student/list/', list_payments, name = 'list_payments'),
-    path('student/list/<str:pk>/', list_payments_for_students, name = 'list_payments_for_students'),
-    path('student/create/', create_payment, name= 'create_payment'),
-    path('student/create/manual/', create_payment_manual, name='create_payment_manual'),
-    path('student/delete/manual/<str:pk>/', delete_payment_manual, name = 'delete_payment_manual'),
+    path('student/list/', list_payments, name='list_payments'),
+    path('student/list/<str:pk>/', list_payments_for_students,
+         name='list_payments_for_students'),
+    path('student/create/', create_payment, name='create_payment'),
+    path('student/create/manual/', create_payment_manual,
+         name='create_payment_manual'),
+    path('student/delete/manual/<str:pk>/',
+         delete_payment_manual, name='delete_payment_manual'),
     # COMPROMISOS DE PAGO
-    path('compromise/create/', create_compromise, name= 'create_compromise'),
+    path('compromise/create/', create_compromise, name='create_compromise'),
+    path('compromise/list/', list_compromises, name='list_compromises')
 ]
