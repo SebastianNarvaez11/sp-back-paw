9ea42f309541780e694ec87b0756010d04cea68e
SebastianNarvaez11 <narvaez.jhoan@correounivalle.edu.co>
2021-02-05 19:43:49 -0500

    delete authentication backens

--- payment/views.py
+++ payment/views.py
@@ -44,7 +44,7 @@ def create_payment(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_200_OK)
 
 
 # Vista para la creacion de pagos manuales
