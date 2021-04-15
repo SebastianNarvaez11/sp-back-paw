43a113e3f204f945febdb507115705e88211f5be
SebastianNarvaez11 <narvaez.jhoan@correounivalle.edu.co>
2021-04-15 16:51:25 -0500

    refactor test performance query pays

--- payment/views.py
+++ payment/views.py
@@ -14,7 +14,7 @@ from users.models import User
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def list_payments(request):
-    pays = Payment.objects.all()
+    pays = Payment.objects.select_related().all()
     serializer = PaymentWhiteStudentSerializer(pays, many=True)
     return Response(serializer.data)
 
@@ -88,7 +88,7 @@ def delete_payment_manual(request, pk):
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def list_compromises(request):
-    compromises = CompromisePay.objects.all()
+    compromises = CompromisePay.objects.select_related().all()
     serializer = CompromiseWithStudentSerializer(compromises, many=True)
     return Response(serializer.data)
 
--- users/views.py
+++ users/views.py
@@ -112,11 +112,9 @@ def list_students_grades(request, grade, schedule):
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def list_students_debt(request):
-    users = User.objects.exclude(deleted=True).exclude(id=request.user.id).exclude(
-        type=1).exclude(type=2).exclude(student__grade=None).exclude(student__coverage=True)
+    users = User.objects.exclude(deleted=True, id=request.user.id, type=1 or 2 , student__grade=None, student__coverage=True).select_related().all()
     serializer = UserStudentDebtSerializer(users, many=True)
     return Response(serializer.data)
-
 
 # CON ESTA VISTA ESTAMOS CREANDO LOS ESTUDIANTES
 @api_view(['POST'])
