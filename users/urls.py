4108a111c5dd00646469603b0ed95bb2dcb5dc0a
SebastianNarvaez11 <narvaez.jhoan@correounivalle.edu.co>
2021-04-18 11:50:26 -0500

    refactor request estadisticas app

--- users/urls.py
+++ users/urls.py
@@ -12,7 +12,8 @@ urlpatterns = [
     path('admin/create/', create_users_admin, name='create_user_admin'),
     path('admin/update/<str:pk>/', update_users_admin, name='update_users_admin'),
     # USUARIOS ESTUDIANTES
-    path('student/list/debt/', list_students_debt, name='list_students_debt'), #listado de estudiantes por meses en mora
+    path('student/list/debt/', list_students_debt, name='list_students_debt'), #listado de estudiantes por meses en mora - estadisticas
+    path('student/list/debt/app/', list_students_debt_app, name='list_students_debt_app'), #listado de estudiantes por meses en mora - APP estadisticas
     path('student/list/<str:grade>/<str:schedule>/', list_students_grades, name='list_students_grades'), #listado de estudiantes filtrado por grado y jornada
     path('student/get/<str:pk>/', get_student_full, name='get_student_full'),
     path('student/list/', list_students, name='list_students'),#necesita un listado que se carga al inicio
